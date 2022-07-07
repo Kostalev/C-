c049d4c02d08b6913339476dcb6c12da6288287b
kostalev <arctic@list.ru>
2022-07-08 01:09:20 +0500

    add Welcome If-Else

---
+++ lesson005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "бен ладен") // (username == "Бен Ладен") чувствительно к регистру. 
+// Добавили ToLower(), для перевода в нижний регистр любого написания
+{
+    Console.WriteLine("Welcome to NY!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
