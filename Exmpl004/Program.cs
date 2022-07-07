f471bf42337cdb05541ea9019d62350904b8667f
kostalev <arctic@list.ru>
2022-07-08 00:12:42 +0500

    add Exmpl004

---
+++ Exmpl004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Для работы с натуральными числами (результат = 6)
+
+// int numberA = 31;
+// int numberB = 5;
+// int div = numberA / numberB;
+// Console.WriteLine(div);
+
+// Для работы с вещественными числами (результат = 6,2)
+
+double numberA = 31;
+double numberB = 5;
+double div = numberA / numberB;
+Console.WriteLine(div);
